cece433cb1982eeba8df5eeaa83215e8b5e6c312
Maximilian Irro <max@disposia.org>
2014-09-17 09:58:47 +0200

    added a OSX lib file. maybe this will come one day to. ZFSonOSX has good progress

---
+++ lib/frachtraum/osx.rb
@@ -0,0 +1,21 @@
+module Frachtraum
+  
+  #
+  # Maybe this will come in time, but it is not a priority.
+  # There is some literature on disk encryption from the command line: 
+  # http://krypted.com/mac-security/encrypting-os-x-mountain-lion/
+  #
+  
+  REQUIRED_TOOLS_OSX = [] # not yet supported
+  
+  def attach_osx(password, depot)
+    # TODO
+    abort "not yet implemented"
+  end
+  
+  def setupdisk_osx(dev, label, password, compression, encryption, keylength, mountpoint)
+    # TODO
+    abort "not yet implemented"
+  end # setupdisk_osx
+  
+end
