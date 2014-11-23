1d6c0f50d49f819750587c328ab6946bb40842ce
Maximilian Irro <max@disposia.org>
2014-11-23 11:39:34 +0100

    0.0.4

--- lib/frachtraum.rb
+++ lib/frachtraum.rb
@@ -14,7 +14,7 @@ require 'frachtraum/osx'
 
 module Frachtraum
   
-  VERSION = '0.0.4-alpha'.freeze
+  VERSION = '0.0.4'.freeze
   
   # Kibibyte, Mebibyte, Gibibyte, etc... all the IEC sizes
   BYTES_IN_KiB = 2**10
